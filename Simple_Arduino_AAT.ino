880061b8e3070dca6feb74e4193b641151148fd1
Nathan <nathan.green@uwaterloo.ca>
2023-12-12 20:00:32 -0500

    Working basic antenna tracker code

--- Simple_Arduino_AAT.ino
+++ Simple_Arduino_AAT.ino
@@ -1,13 +1,19 @@
 #include "src/c_library_v2/ardupilotmega/mavlink.h"
 #include <math.h>
+#include <Servo.h>
+
+Servo pitchL;
+Servo pitchR;
+Servo yaw;
 
 #define PI 3.14159265358979323846
 #define EARTH_RADIUS 6372797.56085
 #define RADIANS PI / 180
 
 //SET ANTENNA TRACKER LOCATION
-#define AAT_LAT //LAT
-#define AAT_LON //LON
+#define AAT_LAT -35.36293361883152
+#define AAT_LON 149.16539769052687
+#define AAT_BEARING 260 //Initial heading when yaw centered, will replace with compass and implement calibration
 
 float vehicle_lat;
 float vehicle_lon;
@@ -21,7 +27,38 @@ double haversine;
 double temp;
 double point_dist;
 
+//PITCH 0 DEG (L, R): 1950, 1050
+//PITCH 90 DEG (L, R): 950, 2050
+//YAW 0 DEG: 1500
+//YAW 85 DEG RIGHT: 600
+//YAW 85 DEG LEFT: 2400
+void setPitchAngle(float angle) {
+  if (angle > 90 || angle < 0) {
+    return;
+  }
+  int microsecondsL;
+  int microsecondsR;
+  microsecondsR = map(angle, 0, 90, 1050, 2050);
+  microsecondsL = (-1 * microsecondsR) + 3000;
+  pitchL.writeMicroseconds(microsecondsL);
+  pitchR.writeMicroseconds(microsecondsR);
+}
+
+void setYawAngle(float angle) {
+  if (angle > 85 || angle < -85) {
+    return;
+  }
+  int microsecondsY;
+  microsecondsY = map(angle, -85, 85, 2400, 600);
+  yaw.writeMicroseconds(microsecondsY);
+}
+
 void setup() {
+  pitchL.attach(10);
+  pitchR.attach(9);
+  yaw.attach(8);
+  setYawAngle(0);
+  setPitchAngle(45);
   Serial.begin(115200);
   Serial1.begin(57600); 
 }
@@ -92,10 +129,50 @@ double calculateBearing(double baseLat, double baseLon, double droneLat, double 
   return bearing;
 }
 
+float pitchAngleCalc(float distance, float altitude) {
+  float out;
+  out = toDegrees(atan(altitude/distance));
+  if (out > 90) {
+    out = 90;
+  }
+  if (out < 0) {
+    out = 0;
+  }
+  return out;
+}
+
+int yawAngleCalc(float bearing) {
+  float upperLimit;
+  float lowerLimit;
+  int out;
+  //bool rollOverRight = false;
+  if (AAT_BEARING > 180) {
+    if (bearing < AAT_BEARING - 85) {
+      bearing = bearing + 360;
+    }
+  } else {
+    if (bearing > AAT_BEARING + 85) {
+      bearing = bearing - 360;
+    }
+  }
+  upperLimit = AAT_BEARING + 85;
+  lowerLimit = AAT_BEARING - 85;
+  out = map(bearing, lowerLimit, upperLimit, -85, 85);
+  if (out > 85) {
+    out = 85;
+  }
+  if (out < -85) {
+    out = -85;
+  }
+  return out;
+}
+
 void loop() {
   readPos();
   dist = calcGPSDist(AAT_LAT, AAT_LON, vehicle_lat, vehicle_lon);
   bear = calculateBearing(AAT_LAT, AAT_LON, vehicle_lat, vehicle_lon);
+  setPitchAngle(pitchAngleCalc(dist, vehicle_alt));
+  setYawAngle(yawAngleCalc(bear));
   Serial.print("Latitude: ");
   Serial.println(vehicle_lat, 6);
   Serial.print("Longitude: ");
@@ -106,6 +183,10 @@ void loop() {
   Serial.println(dist, 2);
   Serial.print("Bearing: ");
   Serial.println(bear, 2);
+  Serial.print("AAT Pitch: ");
+  Serial.println(pitchAngleCalc(dist, vehicle_alt));
+  Serial.print("AAT Yaw: ");
+  Serial.println(yawAngleCalc(bear));
   Serial.print("Incremental: ");
   Serial.println(inc);
   inc = inc+1;
